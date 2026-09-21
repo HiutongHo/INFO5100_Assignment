5679d461ba07e7b9669710a37d49ab8f86b17041
HiutongHo <hiutongho99@gmail.com>
2026-09-21 14:30:27 -0700

    Add files via upload

---
+++ myproject/src/main/java/edu/neu/mgen/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.neu.mgen;
+
+public class HelloWorld {
+    public static void main( String[] args )
+    {
+        System.out.println("It is my first Java program");  
+        
+        // This is class HelloApp
+        /* This is a 
+        multi-line comment 
+         */
+    }
+}
